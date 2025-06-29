23db562ec08189229504c01a1915c0315bb71709
Brandon Jun <arachnite.b@gmail.com>
2025-06-29 00:07:09 -0700

    feat. (TwoSum): updated main file to comply with user input

--- src/P0001_TwoSum/TwoSumMain.java
+++ src/P0001_TwoSum/TwoSumMain.java
@@ -1,14 +1,60 @@
+
 package P0001_TwoSum;
+
+import java.util.Scanner;
 
 public class TwoSumMain {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        int[] result = solution.twoSum(nums, target);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Indices of the two numbers that add up to " + target + ": [" + result[0] + ", " + result[1] + "]");
+        int length = 0;
+        int target = 0;
+        boolean inValidInput = true;
+
+
+        while (inValidInput) {
+            System.out.print("Enter the number of elements in the array: ");
+            try {
+                length = sc.nextInt();
+                inValidInput = false;
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                sc.nextLine(); // Clear the invalid input
+            }
+        }
+        int[] nums = new int[length];
+
+        for (int i = 0; i <= nums.length - 1; i++) {
+
+            inValidInput = true;
+            while (inValidInput) {
+                System.out.print("Enter number " + (i + 1) + ": ");
+                try {
+                    nums[i] = sc.nextInt();
+                    inValidInput = false;
+                } catch (Exception e) {
+                    System.out.println("Invalid input. Please enter a valid integer.");
+                    sc.nextLine(); // Clear the invalid input
+                }
+            }
+        }
+
+        inValidInput = true;
+        while (inValidInput) {
+                System.out.print("Enter target number: ");
+                try {
+                    target = sc.nextInt();
+                    inValidInput = false;
+                } catch (Exception e) {
+                    System.out.println("Invalid input. Please enter a valid integer.");
+                    sc.nextLine(); // Clear the invalid input
+                }
+            }
+
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices of the two numbers that add up to " + target + ": [" + (result[0] + 1) + ", " + (result[1] + 1) + "]");
     }
 }
