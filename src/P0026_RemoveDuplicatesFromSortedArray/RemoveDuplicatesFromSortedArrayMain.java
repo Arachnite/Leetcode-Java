02d9ccd5e720b11707aea3f0ca98dce12ee5368e
Brandon Jun <arachnite.b@gmail.com>
2025-07-04 17:45:48 -0700

    feat. (RemoveDuplicatesFromSortedArray): finished problem 26

--- src/P0026_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArrayMain.java
+++ src/P0026_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArrayMain.java
@@ -1,7 +1,33 @@
 
 package P0026_RemoveDuplicatesFromSortedArray;
 
+import java.util.Scanner;
+
 public class RemoveDuplicatesFromSortedArrayMain {
 
-    
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many numbers you want to use: ");
+        int length = sc.nextInt();
+        int[] nums = new int[length];
+
+        for (int i = 0; i <= nums.length - 1; i++) {
+
+            System.out.print("Enter number " + (i + 1) + ": ");
+            nums[i] = sc.nextInt();
+
+        }
+
+        length = solution.removeDuplicates(nums);
+
+        System.out.print("New array: ");
+        for (int i = 0; i < length; i++) {
+
+            System.out.print(nums[i] + ", ");
+
+        }
+    }
 }
--- src/P0026_RemoveDuplicatesFromSortedArray/Solution.java
+++ src/P0026_RemoveDuplicatesFromSortedArray/Solution.java
@@ -2,4 +2,20 @@
 package P0026_RemoveDuplicatesFromSortedArray;
 
 public class Solution {
+
+    public int removeDuplicates(int[] nums) {
+
+        int index = 1;
+        for (int i = 1; i <= nums.length - 1; i++) {
+
+            if (nums[i - 1] != nums[i]) {
+
+                nums[index] = nums[i];
+                index++;
+
+            }
+        }
+
+        return index;
+    }
 }
