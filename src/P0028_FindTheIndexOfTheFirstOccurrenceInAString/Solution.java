9967fc5c201f0526117c2557d61579f78f248460
Brandon Jun <arachnite.b@gmail.com>
2025-07-09 22:54:05 -0700

    feat. (FindTheIndexOfTheFirstOccurenceInAString): finished problem 28 solution

--- src/P0028_FindTheIndexOfTheFirstOccurrenceInAString/Solution.java
+++ src/P0028_FindTheIndexOfTheFirstOccurrenceInAString/Solution.java
@@ -3,6 +3,31 @@ package P0028_FindTheIndexOfTheFirstOccurrenceInAString;
 
 public class Solution {
 
-    
+    public int strStr(String haystack, String needle) {
 
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+
+            if (haystack.charAt(i) == needle.charAt(0)) {
+
+                boolean match = true;
+                for (int ii = 1; ii <= needle.length() - 1; ii++) {
+
+                    if (haystack.charAt(i + ii) != needle.charAt(ii)) {
+
+                        match = false;
+                        break;
+
+                    }
+                }
+
+                if (match) {
+
+                    return i;
+
+                }
+            }
+        }
+
+        return -1;
+    }
 }
