33292d877492a8e2d5a1d11a60761c7c5a63ec08
Brandon Jun <arachnite.b@gmail.com>
2025-07-13 02:54:13 -0700

    feat. (AddTwoNumbers): IM CRAZY

--- src/P0002_AddTwoNumbers/Main.java
+++ src/P0002_AddTwoNumbers/Main.java
@@ -1,4 +1,59 @@
+
 package P0002_AddTwoNumbers;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        String numOne = String.valueOf(num1);
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        String numTwo = String.valueOf(num2);
+
+        ListNode numberOne = new ListNode();
+        ListNode oneHead = numberOne;
+        ListNode numberTwo = new ListNode();
+        ListNode twoHead = numberTwo;
+
+        for (int i = 0; i <= numOne.length() - 1; i++) {
+            numberOne.val = Integer.parseInt(String.valueOf(numOne.charAt(i)));
+            if (i < numOne.length() - 1) {
+                numberOne.next = new ListNode();
+                numberOne = numberOne.next;
+            }
+        }
+
+        for (int i = 0; i <= numTwo.length() - 1; i++) {
+            numberTwo.val = Integer.parseInt(String.valueOf(numTwo.charAt(i)));
+            if (i < numOne.length() - 1) {
+                numberTwo.next = new ListNode();
+                numberTwo = numberTwo.next;
+            }
+        }
+
+        ListNode result = solution.addTwoNumbers(oneHead, twoHead);
+
+        System.out.print("Result: ");
+
+        printNum(result);
+
+    }
+
+    public static void printNum(ListNode head) {
+
+        if (head == null) {
+            return;
+        }
+
+        System.out.print(head.val);
+        printNum(head.next);
+    }
 }
--- src/P0002_AddTwoNumbers/Solution.java
+++ src/P0002_AddTwoNumbers/Solution.java
@@ -1,4 +1,104 @@
+
 package P0002_AddTwoNumbers;
 
 public class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode l3 = new ListNode();
+        ListNode head = l3;
+        boolean carry = false;
+
+        outerloop:
+        while (true) {
+
+            l3.val = l1.val + l2.val;
+
+            if (carry) {
+                l3.val += 1;
+                carry = false;
+            }
+
+            if (l3.val >= 10) {
+                l3.val -= 10;
+                carry = true;
+            }
+
+            if (l1.next == null && l2.next == null) {
+                if (carry) {
+                    l3.next = new ListNode(1);
+                }
+                break outerloop;
+
+            } else if (l1.next == null) {
+                l2 = l2.next;
+                l3.next = new ListNode();
+                l3 = l3.next;
+
+                while (true) {
+
+                    l3.val = l2.val;
+
+                    if (carry) {
+                        l3.val++;
+                        carry = false;
+                    }
+
+                    if (l3.val >= 10) {
+                        l3.val -= 10;
+                        carry = true;
+                    }
+
+                    if (l2.next == null) {
+                        if (carry) {
+                            l3.next = new ListNode(1);
+                        }
+                        break outerloop;
+                    } else {
+                        l2 = l2.next;
+                        l3.next = new ListNode();
+                        l3 = l3.next;
+                    }
+                }
+            } else if (l2.next == null) {
+                l1 = l1.next;
+                l3.next = new ListNode();
+                l3 = l3.next;
+
+                while (true) {
+
+                    l3.val = l1.val;
+
+                    if (carry) {
+                        l3.val++;
+                        carry = false;
+                    }
+
+                    if (l3.val >= 10) {
+                        l3.val -= 10;
+                        carry = true;
+                    }
+
+                    if (l1.next == null) {
+                        if (carry) {
+                            l3.next = new ListNode(1);
+                        }
+                        break outerloop;
+                    } else {
+                        l1 = l1.next;
+                        l3.next = new ListNode();
+                        l3 = l3.next;
+                    }
+                }
+
+            } else {
+                l1 = l1.next;
+                l2 = l2.next;
+                l3.next = new ListNode();
+                l3 = l3.next;
+            }
+        }
+
+        return head;
+    }
 }
