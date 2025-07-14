1576b6afb7f6621f3c16f99530391cb40a9d001a
Brandon Jun <arachnite.b@gmail.com>
2025-07-14 03:50:13 -0700

    feat. (MedianOfTwoSortedArrays): finished solution

---
+++ src/P0004_MedianOfTwoSortedArrays/Main.java
@@ -0,0 +1,13 @@
+
+package P0004_MedianOfTwoSortedArrays;
+
+public class Main {
+
+    public static void main(String [] args) {
+
+        Solution solution = new Solution();
+
+        solution.findMedianSortedArrays(new int[] {1, 2}, new int[] {3, 4});
+
+    }
+}
--- src/P0004_MedianOfTwoSortedArrays/Solution.java
+++ src/P0004_MedianOfTwoSortedArrays/Solution.java
@@ -5,51 +5,49 @@ public class Solution {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        int index1; int index2;
-        double result1 = 0; double result2 = 0;
+        int[] merged = new int[nums1.length + nums2.length];
 
-        if ((nums1.length != 0)) {
-            if (nums1.length % 2 == 1) {
+        int i = 0, j = 0, k = 0;
 
-                index1 = (int)((nums1.length / 2) + 0.5);
-                result1 = nums1[index1];
+        while (i < nums1.length && j < nums2.length) {
 
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
             } else {
-
-                index1 = (nums1.length / 2);
-                if (index1 == 0) {
-                    result1 = nums1[0];
-                } else {
-                    result1 = (nums1[index1 - 1] + nums1[index1]) / 2.0;
-                }
+                merged[k++] = nums2[j++];
             }
+
         }
 
-        if ((nums2.length != 0)) {
-            if (nums2.length % 2 == 1) {
+        while (i < nums1.length) {
 
-                index2 = (int)((nums2.length / 2) + 0.5);
-                result2 = nums2[index2];
+            merged[k++] = nums1[i++];
 
-            } else {
-
-                index2 = (nums2.length / 2);
-                if (index2 == 0) {
-                    result2 = nums2[0];
-                } else {
-                    result2 = (nums2[index2 - 1] + nums2[index2]) / 2.0;
-                }
-            }
         }
 
-        if (result1 == 0 && result2 == 0) {
-            return 0;
-        } else if (result1 == 0) {
-            return result2;
-        } else if (result2 == 0) {
-            return result1;
+        while (j < nums2.length) {
+
+            merged[k++] = nums2[j++];
+
+        }
+
+        if (merged.length == 0) {
+
+            return 0.0;
+
+        } else if (merged.length == 1) {
+
+            return merged[0];
+
+        } else if (merged.length % 2 == 0) {
+
+            int index = merged.length / 2;
+            return (merged[index - 1] + merged[index]) / 2.0;
+
         } else {
-            return (result1 + result2) / 2.0;
+
+            return merged[merged.length / 2];
+
         }
     }
 }
