79db0db809d542e9ca91dbb1683637e69dae3278
Brandon Jun <arachnite.b@gmail.com>
2025-09-18 09:48:03 -0700

    feat. (BinaryTreePostorderTraversal): started on solution class

---
+++ src/P0145_BinaryTreePostorderTraversal/Solution.java
@@ -0,0 +1,14 @@
+package P0145_BinaryTreePostorderTraversal;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Solution {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<Integer>();
+
+        return result;
+    }
+}
